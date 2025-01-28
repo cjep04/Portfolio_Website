a272cb04856a780c6c44233904a09ce193716b26
cjep04 <charlie@quickbrandlaunch.com>
2025-01-28 15:37:56 +0000

    responisve design

--- docs/JS/index.js
+++ docs/JS/index.js
@@ -135,3 +135,16 @@ function toggleMenu() {
     const navItems = document.querySelector('.nav_items');
     navItems.classList.toggle('show');
 }
+
+const toggleBtn = document.querySelector('.toggle_btn');
+            const toggleBtnIcon = document.querySelector('.toggle_btn i');
+            const DropDownMenu = document.querySelector('.dropdown_menu');
+    
+            toggleBtn.onclick = function () {
+                DropDownMenu.classList.toggle("open");
+                const isOpen = DropDownMenu.classList.contains('open')
+
+                toggleBtnIcon.classList = isOpen
+                ?   'fa-solid fa-xmark'
+                :   'fa-solid fa-bars'
+            };
